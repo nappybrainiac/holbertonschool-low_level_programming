6efd3d40682fccf2d4ed5d2c695f8bd17f1884ad
Gloria Bwandungi <30@holbertonschool.com>
2016-02-14 23:36:03 -0800

    Not done yet

---
+++ dreams_and_dedication_are_a_powerful_combination/0-print_number.c
@@ -0,0 +1,55 @@
+#include "my_functions.h"
+
+/* Main Function */
+
+void print_number (int n) {
+
+  /* Declaring variables*/
+
+  int numStore;
+  int i = n;
+  int num = n;
+  int num_re = n;
+  int power = 10;
+  int count;
+  int num_p;
+ 
+
+
+  /* if statements */
+ 
+  if (n == 0) {
+    print_char('0');
+  }
+
+  if (n < 0) {
+    print_char('-');
+    numStore = n * -1;
+    i = n * -1;
+    num = n * -1;
+  }
+
+  /* The Loops */
+
+
+  /* The Count */
+
+  while(num_re > 0) {             /* To find out what multiple of 10 should be used */
+    num_re= numStore / power;
+    power = power * 10;
+  }
+  power = power / 10;
+  
+  while(i > 0) {                    /* To find out how many times the print loop should run */
+  i = i / 10;
+  count = count + 1;
+ }
+
+  while(count > 0) {                 /* To print the numbers one by one */
+  num_p = num / power;
+  print_char(48 + num_p);
+  num = num - (num_p * 10);
+  power = power/10;
+  count = count - 1;
+ } 
+}
