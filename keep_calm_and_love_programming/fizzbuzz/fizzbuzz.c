9526c9d7b8b035e83258736950f59bbec041fb45
Gloria Bwandungi <30@holbertonschool.com>
2016-02-19 17:29:54 -0500

    Comments added

--- keep_calm_and_love_programming/fizzbuzz/fizzbuzz.c
+++ keep_calm_and_love_programming/fizzbuzz/fizzbuzz.c
@@ -1,26 +1,37 @@
 #include <stdio.h>
 
+/*The main function cannot return anything other
+  than an int.
+  This is the famous fizzbuzz program. It prints Fizz
+  if the number is divisible by 3, Buzz if it is divisible 
+  by 5, and FizzBuzz if it is divisible by both
+  3 and 5.
+*/
+
 int main(void)
 {
 
+  /* Variable Declaration */
   int num = 1;
+
+
   for(num = 1; num <= 100; num++)
     {
-      if(num % 3 == 0 && num % 5 == 0)
+      if(num % 3 == 0 && num % 5 == 0) /* if num is divisible by both 3 and 5 */
 	{
 	  printf("FizzBuzz ");
 	}
-      else if(num % 3 == 0)
+      else if(num % 3 == 0)            /* if num is divisible by 3 */
 	{
 	  printf("Fizz ");
 	}
-      else if(num % 5 == 0)
+      else if(num % 5 == 0)            /* if num is divisible by 5 */
 	{
 	  printf("Buzz ");
 	}
       else
 	{
-	  printf("%d ", num);
+	  printf("%d ", num);          /* otherwise print the number*/
 	}  
     }
   return (0);
