eaf3d95828d36c89065a86dc2ad0e9db23380693
Gloria Bwandungi <30@holbertonschool.com>
2016-02-19 17:19:02 -0500

    FizzBuzz Buzzes Fizzily complete

---
+++ keep_calm_and_love_programming/fizzbuzz/fizzbuzz.c
@@ -0,0 +1,27 @@
+#include <stdio.h>
+
+int main(void)
+{
+
+  int num = 1;
+  for(num = 1; num <= 100; num++)
+    {
+      if(num % 3 == 0 && num % 5 == 0)
+	{
+	  printf("FizzBuzz ");
+	}
+      else if(num % 3 == 0)
+	{
+	  printf("Fizz ");
+	}
+      else if(num % 5 == 0)
+	{
+	  printf("Buzz ");
+	}
+      else
+	{
+	  printf("%d ", num);
+	}  
+    }
+  return (0);
+}
