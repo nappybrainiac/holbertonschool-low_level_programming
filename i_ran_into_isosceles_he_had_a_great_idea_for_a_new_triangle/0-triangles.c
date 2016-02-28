6e90c2ab0e61a29de74f7ed0518744cdf6fab872
Gloria Bwandungi <30@holbertonschool.com>
2016-02-28 05:09:52 -0500

    Comments added

--- i_ran_into_isosceles_he_had_a_great_idea_for_a_new_triangle/0-triangles.c
+++ i_ran_into_isosceles_he_had_a_great_idea_for_a_new_triangle/0-triangles.c
@@ -7,15 +7,15 @@ void triangles_in_the_term(int h, int n){
   int lines = 0;
   int nSave = 0;
 
-  for(nSave = 1; nSave <= n; nSave++){
-    for(lines = 1; lines <= h; lines++){
-      for(spaces = h - lines; spaces >= 1; spaces--){
+  for(nSave = 1; nSave <= n; nSave++){                       /*Determines the number of triangles to be printed*/
+    for(lines = 1; lines <= h; lines++){                     /*Determines the number of lines per triangle to be printed*/
+      for(spaces = h - lines; spaces >= 1; spaces--){        /*Determines the number of spaces per line*/
 	printf(" ");
       }
-      for(stars = 1; stars <= ((lines * 2) - 1); stars++){
+      for(stars = 1; stars <= ((lines * 2) - 1); stars++){   /*Determines the number of stars per line*/
 	printf("*");
       }
-      printf("\n");
+      printf("\n");                                          /*New line*/
     }
   }
 }
