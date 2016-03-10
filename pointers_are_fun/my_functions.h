733901f3baf1c59971c8349279c8833d1d3303bf
Gloria Bwandungi <30@holbertonschool.com>
2016-03-09 22:56:17 -0500

    missing library file

---
+++ pointers_are_fun/my_functions.h
@@ -0,0 +1,10 @@
+/* This library has been specifically created
+   to handle the requests of the .c files in 
+   this repository.*/
+
+void reset_to_98(int *n);      /*To reset the value of an integer to 98*/
+int print_char(char c);   
+void swap_int(int *a, int *b); /*To swap the values of two integers*/
+int str_len(char *s);          /*Finds the length of a string*/
+void print_string(char *str);  /*Prints a string*/
+void reverse_string(char *s);  /*To print a string in reverse*/
