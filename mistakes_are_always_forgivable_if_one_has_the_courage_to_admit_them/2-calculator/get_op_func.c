4b0f2cb352facda816c07ecd25733eae23c31bc5
Gloria Bwandungi <Gloria.Bwandungi@holbertonschool.com>
2016-04-09 04:26:31 +0000

    Assignment corrected for op_mul & op_div

--- mistakes_are_always_forgivable_if_one_has_the_courage_to_admit_them/2-calculator/get_op_func.c
+++ mistakes_are_always_forgivable_if_one_has_the_courage_to_admit_them/2-calculator/get_op_func.c
@@ -20,8 +20,8 @@ int (*get_op_func(char c))(int, int){
   /*Array that holds pointers to functions*/
   funz[0] = op_add;
   funz[1] = op_sub;
-  funz[2] = op_mul;
-  funz[3] = op_div;
+  funz[2] = op_div;
+  funz[3] = op_mul;
   funz[4] = op_mod;
 
   /*The arrays have been arranged to correspond 
@@ -29,7 +29,7 @@ int (*get_op_func(char c))(int, int){
   for(i = 0; i < ops[i]; i++){
     if(c == ops[i]){
 	return funz[i];
-      }
+    }
   }
   return 0;
 }
