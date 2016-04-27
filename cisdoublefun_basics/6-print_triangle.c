f0768147f7dfa16dfec2b90b41f7b7bc0bd1b5bb
Gloria Bwandungi <Gloria.Bwandungi@holbertonschool.com>
2016-04-27 11:22:59 -0400

    Comments added

--- cisdoublefun_basics/6-print_triangle.c
+++ cisdoublefun_basics/6-print_triangle.c
@@ -7,7 +7,7 @@ void print_triangle(int n){
   if(n <= 0){
     print_char('0');
   }
-  while(lines <= n){
+  while(lines <= n){         /* Prints the top half of the triangle */
     int height = 1;
     while(height <= lines){
       print_char('*');
@@ -16,7 +16,7 @@ void print_triangle(int n){
     print_char('\n');
     lines++;
   }
-  while(lines2 > 0){
+  while(lines2 > 0){          /* Prints the bottom half of the triangle */
     int height2 = lines2;
     while(height2 > 0){
       print_char('*');
