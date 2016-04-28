6137ae869e64e535d9fe67e918e21d50716984db
Gloria Bwandungi <Gloria.Bwandungi@holbertonschool.com>
2016-04-28 17:48:51 -0400

    my_functions.h added

---
+++ cisdoublefun_day_2_pointers/my_functions.h
@@ -0,0 +1,7 @@
+int print_char(char c);            /* Prints one single character */
+void swap_int(int *a, int *b);     /* Swaps the values of two numbers */
+void print_string(char *str);      /* Prints a string using pointers */
+int str_len(char *s);
+char *rev_string(char *str);
+int string_to_integer(char *s);
+void sort_array(int *a, int n);
