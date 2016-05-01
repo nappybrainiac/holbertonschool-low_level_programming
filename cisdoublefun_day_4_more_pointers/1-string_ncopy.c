44e47ecc5260dbf6b04c54000aebacad3aae1645
Gloria Bwandungi <Gloria.Bwandungi@holbertonschool.com>
2016-05-01 00:18:14 -0400

    Comment

--- cisdoublefun_day_4_more_pointers/1-string_ncopy.c
+++ cisdoublefun_day_4_more_pointers/1-string_ncopy.c
@@ -4,7 +4,7 @@ char *string_ncopy(char *dest, const char *src, int n){
 
   while((*(src + i) - 1) != '\0' && n > 0){
 
-    dest[i] = src[i];
+    dest[i] = src[i];   /* Replace each dest with src values */
     i++;
     n--;
   }
