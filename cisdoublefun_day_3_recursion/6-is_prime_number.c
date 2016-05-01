6bcd77957aa7a48aca62d7cc97c806fcc66b78f7
Gloria Bwandungi <Gloria.Bwandungi@holbertonschool.com>
2016-04-30 22:26:17 -0400

    Comments Added

--- cisdoublefun_day_3_recursion/6-is_prime_number.c
+++ cisdoublefun_day_3_recursion/6-is_prime_number.c
@@ -1,8 +1,8 @@
 int prime_number(int n, int i){  /* This is the recursive function */
 
-  if(i == 1){
+  if(i == 1){   /* When i reaches 1, then it means n is prime */
     return 1;
-  } else if (n % i == 0){
+  } else if (n % i == 0){  /* If n is divisible by any value of i */
     return 0;
   }
   return prime_number(n, (i - 1));
