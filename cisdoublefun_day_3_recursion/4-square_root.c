9523e03c763808b949e505380fe1cd9917e7b95e
Gloria Bwandungi <Gloria.Bwandungi@holbertonschool.com>
2016-05-01 11:14:09 -0400

    In the case of 0

--- cisdoublefun_day_3_recursion/4-square_root.c
+++ cisdoublefun_day_3_recursion/4-square_root.c
@@ -1,13 +1,13 @@
 int square_root(int n){
   int i;             /* Counter for while loop */
 
-  i = n/2;
+  i = n/2;           /* Start at 1/2 of n */
   if(n < 0) {
     return -1;
-  } else if (n > 2147483600) {
+  } else if (n > 2147483600) {  /* For extreme cases */
     return -1;
-  } else if (n == 1) {
-    return 1;
+  } else if (n == 1 || n == 0) { /* The square root of 1 is 1*/
+    return n;
   }
 
   while(i > 0){
