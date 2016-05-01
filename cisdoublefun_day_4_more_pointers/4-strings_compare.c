7618b36f9ec1e016d085a0810406061d467403fd
Gloria Bwandungi <Gloria.Bwandungi@holbertonschool.com>
2016-05-01 10:07:45 -0400

    Comments added

--- cisdoublefun_day_4_more_pointers/4-strings_compare.c
+++ cisdoublefun_day_4_more_pointers/4-strings_compare.c
@@ -3,14 +3,14 @@ int strings_compare(char *s1, const char *s2){
   int i = 0;
   int j = 0;
 
-  while(s1[i] != '\0'){
+  while(s1[i] != '\0'){ /* Find the length of the first string */
     i++;
   }
-  while(s2[j] != '\0'){
+  while(s2[j] != '\0'){ /* Find the length of the second string */
     j++;
   }
 
-  if(i > j){
+  if(i > j){            /* Using the definition of what strcmp is ...*/
     return 15;
   }else if (i < j){
     return -15;
