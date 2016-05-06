aaba8462ccb83dd799425ee904c9a1c2fcecef06
Gloria Bwandungi <Gloria.Bwandungi@holbertonschool.com>
2016-05-06 13:46:59 +0000

    Commented

--- macros_and_structures/0-abs.h
+++ macros_and_structures/0-abs.h
@@ -1,2 +1,9 @@
-/* This macro returns the absolute value of a number provided */
+/*
+ *     0-abs.h
+ *
+ *     This header file contains one macro that returns the
+ *     absolute value of a given integer.
+ *
+ *     by Gloria Bwandungi. 
+ */
 #define ABS(d) d < 0 ? (d * -1): d
