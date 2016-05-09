75ad238be78175fefddedb56a21daaf7a420ac78
Gloria Bwandungi <Gloria.Bwandungi@holbertonschool.com>
2016-05-09 22:55:34 +0000

    Task 0, Makefile in the works

--- file_descriptors/0-print_content/0-print_content.c
+++ file_descriptors/0-print_content/0-print_content.c
@@ -8,3 +8,31 @@
  *
  *     by Gloria Bwandungi, 2016.
  */
+
+
+#include <fcntl.h>
+#include <unistd.h>
+#include <stdio.h>
+
+int main(int argc, char *argv[]){
+  int i;
+  int fd;
+  char buf[BUFSIZ];
+
+  if(argc < 2){
+    return 1;
+  }
+
+  fd = open(argv[1], O_RDONLY);  /* saves the file descriptor in
+                                  * a variable (fd) that can now
+                                  * be manipulated with other
+                                  * commands
+                                  */
+
+  while((i = read(fd, buf, BUFSIZ)) > 0){
+    write(1, buf, i);
+  }
+
+close(fd);
+return 0;
+}
