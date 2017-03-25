24e8ef5b003cc9e8ba2f1cb9b6b1d88155269571
Gloria Bwandungi <30@holbertonschool.com>
2017-03-25 19:25:37 -0400

    Task 0 Complete, checking...

---
+++ 0x12-more_singly_linked_lists/0-print_listint.c
@@ -0,0 +1,22 @@
+#include <stdio.h>
+#include "lists.h"
+
+/*
+ * linstint_t - singly linked lists
+ * @h: head of the list (first node)
+ * @next: next node in the lists
+ *
+ * This function returns the number of nodes in the
+ * provided linked list.
+ */
+size_t print_listint(const listint_t *h)
+{
+    size_t i = 0;
+    while (h != NULL)
+    {
+        printf("%d\n", h->n);
+        h = h->next;
+        i++;
+    }
+    return (i);
+}
