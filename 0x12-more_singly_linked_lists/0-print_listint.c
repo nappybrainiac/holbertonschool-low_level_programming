388195d5d8e26cd7de592b3e70ef5a3e95d0018d
Gloria Bwandungi <30@holbertonschool.com>
2017-03-25 19:56:13 -0400

    Betty Checked...

--- 0x12-more_singly_linked_lists/0-print_listint.c
+++ 0x12-more_singly_linked_lists/0-print_listint.c
@@ -11,12 +11,13 @@
  */
 size_t print_listint(const listint_t *h)
 {
-  size_t i = 0;
-  while (h != NULL)
-    {
-      printf("%d\n", h->n);
-      h = h->next;
-      i++;
-    }
-  return (i);
+	size_t i = 0;
+
+	while (h != NULL)
+	{
+		printf("%d\n", h->n);
+		h = h->next;
+		i++;
+	}
+	return (i);
 }
