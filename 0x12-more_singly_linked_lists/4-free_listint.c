15f3800509127be9c2cff0340accfa1205358d56
Gloria Bwandungi <30@holbertonschool.com>
2017-03-27 18:04:54 -0400

    Task 4 Complete. Betty Checked.

---
+++ 0x12-more_singly_linked_lists/4-free_listint.c
@@ -0,0 +1,14 @@
+#include <stdlib.h>
+#include "lists.h"
+
+/* to free the memory being held by a linked list*/
+void free_listint(listint_t *head)
+{
+	listint_t *current = NULL;
+
+	while ((current = head) != NULL)
+	{
+		head = head->next;
+		free(current);
+	}
+}
