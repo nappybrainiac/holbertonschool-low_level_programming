daf8178d7aa61aa83c1ff11dc420859715178c2f
Gloria Bwandungi <30@holbertonschool.com>
2017-03-30 13:58:26 -0400

    Task 9, Seg fault

--- 0x12-more_singly_linked_lists/9-insert_nodeint.c
+++ 0x12-more_singly_linked_lists/9-insert_nodeint.c
@@ -51,7 +51,7 @@ listint_t *insert_nodeint_at_index(listint_t **head, unsigned int idx, int n)
 			}
 			/* break connections between nodes where new node should go */
 
-			/*Zee error is in heeya... current -> new -> post*/
+			/* I sink, Zee error is in heeya... current -> new -> post*/
 			current->next = new_node;
 			new_node->next = post_node;
 			}
