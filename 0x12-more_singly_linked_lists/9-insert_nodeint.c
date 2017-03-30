921310896c09bcc0300874aae833f2731b7628dc
Gloria Bwandungi <30@holbertonschool.com>
2017-03-30 18:34:09 -0400

    changed return of new_node

--- 0x12-more_singly_linked_lists/9-insert_nodeint.c
+++ 0x12-more_singly_linked_lists/9-insert_nodeint.c
@@ -23,7 +23,7 @@ listint_t *insert_nodeint_at_index(listint_t **head, unsigned int idx, int n)
 	new_node->n = n;
 	new_node->next = NULL;
 	/* does the list exist? Is the index +ve whole number? */
-	if (head == NULL)
+	if (*head == NULL)
 	{
 		*head = new_node;
 		return (new_node);
