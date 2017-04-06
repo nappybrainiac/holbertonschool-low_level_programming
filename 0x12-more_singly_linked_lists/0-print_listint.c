dac6007614d6ac9cdb16a631abe9a291b2a4ba92
Gloria Bwandungi <30@holbertonschool.com>
2017-04-06 10:32:30 -0400

    trial 2

--- 0x12-more_singly_linked_lists/0-print_listint.c
+++ 0x12-more_singly_linked_lists/0-print_listint.c
@@ -1,16 +1,19 @@
+#include <stdlib.h>
 #include <stdio.h>
 #include "lists.h"
 
 /**
- * print_listint - function to print and returns # of nodes in a list
- * @h: pointer to the head of a list
+ * print_listint - function that prints the values of each node and
+ * returns the number of elements in a linked listint_t list.
+ * @h: The head node in the linked list.
  *
- * Return: counts the elements of a listint_t list
+ * Return: The size of the linked list or node count.
  */
-
 size_t print_listint(const listint_t *h)
 {
-	size_t count = 0;
+	size_t count;
+
+	count = 0;
 
 	while (h != NULL)
 	{
@@ -18,6 +21,5 @@ size_t print_listint(const listint_t *h)
 		h = h->next;
 		count++;
 	}
-
 	return (count);
 }
