5d7f269a91fb0c1463002f4caa123f41ff30fade
Gloria Bwandungi <30@holbertonschool.com>
2017-04-06 10:31:03 -0400

    trial 2

--- 0x12-more_singly_linked_lists/0-print_listint.c
+++ 0x12-more_singly_linked_lists/0-print_listint.c
@@ -2,22 +2,22 @@
 #include "lists.h"
 
 /**
- * print_listint - to print the values in the list.
- * @h: Current head node of type struct listint_t.
+ * print_listint - function to print and returns # of nodes in a list
+ * @h: pointer to the head of a list
  *
- * Return: size_t.
+ * Return: counts the elements of a listint_t list
  */
 
- size_t print_listint(const listint_t *h)
- {
- 	size_t count = 0;
+size_t print_listint(const listint_t *h)
+{
+	size_t count = 0;
 
- 	while (h != NULL)
- 	{
- 		printf("%d\n", h->n);
- 		h = h->next;
- 		count++;
- 	}
+	while (h != NULL)
+	{
+		printf("%d\n", h->n);
+		h = h->next;
+		count++;
+	}
 
- 	return (count);
- }
+	return (count);
+}
