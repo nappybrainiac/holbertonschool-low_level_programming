91d52eae1a8ac746c4c9ac40a843a7b7b34c68da
Gloria Bwandungi <30@holbertonschool.com>
2017-04-05 23:29:24 -0400

    Checker trial

--- 0x12-more_singly_linked_lists/10-delete_nodeint.c
+++ 0x12-more_singly_linked_lists/10-delete_nodeint.c
@@ -13,32 +13,32 @@
 
 int delete_nodeint_at_index(listint_t **head, unsigned int index)
 {
-	  listint_t *current;
-	  listint_t *del_node;
-	  unsigned int i = 0; /* iterator */
+	listint_t *current;
+	listint_t *del_node;
+	unsigned int i = 0; /* iterator */
 
-	  current = *head;
-	  if (*head == NULL)
-	  {
-		  return (-1);
-	  }
-	  while (current != NULL) /* iterate through the list */
-	  {
-		  if (index == 0)
-		  {
-			  *head = (*head)->next;
-			  free(current);
-			  return (1);
-		  }
-		  else if (i == (index - 1))
-		  {
-			  del_node = current->next;
-			  current->next = current->next->next;
-			  free(del_node);
-			  return (1);
-		  }
-		  current = current->next;
-		  i++;
-	  }
-	  return (-1);
+	current = *head;
+
+	if (*head == NULL)
+	{
+		return (-1);
+	}
+	while (current != NULL) /* iterate through the list */
+	{
+		if (index == 0)
+		{
+			*head = (*head)->next;
+			free(current);
+			return (1);
+		}
+		else if (i == (index - 1))
+		{
+			del_node = current->next;
+			current->next = current->next->next;
+			free(del_node);
+		}
+		current = current->next;
+		i++;
+	}
+	return (1);
 }
