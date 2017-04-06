cbd24225a84ec88b9850667798847e00121cdb36
Gloria Bwandungi <30@holbertonschool.com>
2017-04-06 09:00:50 -0400

    using count instead of i to iterate

--- 0x12-more_singly_linked_lists/0-print_listint.c
+++ 0x12-more_singly_linked_lists/0-print_listint.c
@@ -7,15 +7,17 @@
  *
  * Return: size_t.
  */
-size_t print_listint(const listint_t *h)
-{
-	size_t i = 0;
 
-	while (h != NULL)
-	{
-		printf("%d\n", h->n);
-		h = h->next;
-		i++;
-	}
-	return (i);
-}
+ size_t print_listint(const listint_t *h)
+ {
+ 	size_t count = 0;
+
+ 	while (h != NULL)
+ 	{
+ 		printf("%d\n", h->n);
+ 		h = h->next;
+ 		count++;
+ 	}
+
+ 	return (count);
+ }
