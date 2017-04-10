fe36e38b2a0d590e98ab415bac71bff866c5f678
Gloria Bwandungi <30@holbertonschool.com>
2017-04-10 00:03:42 -0400

    Tasks 0 & 1 Complete. Betty Checking...

---
+++ printf/printf/holberton.h
@@ -0,0 +1,13 @@
+#ifndef LISTS
+#define LISTS
+
+/**/
+
+int print_char(char c);
+void print_number(int n);
+int _printf(const char *format, ...);
+char *convert(unsigned int num, int base);
+int str_len(char *s);
+void print_string(char *str);
+
+#endif /* LISTS */
